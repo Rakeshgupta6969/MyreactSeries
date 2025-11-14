bc5c891c6dfa1fcf1ee9122cdb9c2af8a2040251
Rakesh Gupta <rg688407@gmail.com>
2025-11-14 21:31:50 +0530

    mega Project Start

---
+++ MegaProject/src/App.jsx
@@ -0,0 +1,17 @@
+
+import './App.css'
+
+function App() {
+    
+   console.log(import.meta.env.VITE_APPWRITE_URL);
+
+  return (
+    <>
+      <div>
+        <h1> A   Mega Project with appWrite for the Backend integration.</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
---
+++ MegaProject/src/AppWrite/Auth.js
@@ -0,0 +1,35 @@
+import Conf from "../Config/Conf";
+import { Client, Account, ID } from "appwrite";
+
+
+
+
+export class AuthService{
+    
+    client = new Client();
+    account;
+
+    constructor(){
+        this.client
+        .setEndpoint(Conf. appWriteUrl)
+        .setProject(Conf.appWriteProjectId)
+
+        this.account = new Account(this.client)
+    }
+
+
+    async createAccount({email,password,name}){
+        try{
+         const userAccount   =      await this.account.create(ID.unique(),email,password,name);
+        }
+        catch(error){
+             throw error;
+        }
+    }
+
+}
+
+
+const authService = new AuthService();
+
+export default authService;
